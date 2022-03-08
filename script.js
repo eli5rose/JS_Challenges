47fd2884130c9c22f8f9e8ed7a12ee108cc6f56e
Elena A Chobanova <97087759+eli5rose@users.noreply.github.com>
2022-03-08 12:01:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let name ="John";
+let admin = name;
+console.log(admin);
+
+
+let a = 1; 
+a++;
+console.log(a);
+
+let c = ++a;
+console.log(c);
+
+let b = 1;
+console.log(b);
+
+let d = b++;
+console.log(b);
+
